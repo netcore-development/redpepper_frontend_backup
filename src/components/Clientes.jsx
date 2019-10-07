f904097bc41c13b48be3f920e750cfef4bf8fd68
bgarcia95 <bgarcia.95@outlook.com>
2019-10-07 17:55:58 -0600

    Tabla agregada y llenada con informacion de DB

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -19,28 +19,53 @@ class Clientes extends Component {
         <div id="contenedorClientes" className="container">
           <div className="row">
             <div className="col-md-12 center-block text-center py-5">
-              <div className="card py-5">
-                <p>Lista de Clientes</p>
-                {values.map(cliente => (
-                  <Fragment>
-                    <div className="row">
-                        <div className="col-md-4"/>
-                      <div className="col-md-2">
-                        <p key={cliente.id}>Id: {cliente.id}</p>
-                      </div>
-                      <div className="col-md-3">
-                        <p key={cliente.id}>
-                          Nombres: {cliente.persona.nombres}
-                        </p>
-                      </div>
-                      <div className="col-md-3"/>
-                    </div>
-                  </Fragment>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
+              <div className="card py-5 px-5">
+                <h2 style={{ textDecoration: "underline" }}>
+                  Lista de Clientes
+                </h2>
+                <br />
+                <table
+                  id="example"
+                  className="table table-striped table-bordered"
+                  // style={{ width: "100%" }}
+                >
+                  <thead>
+                    <tr>
+                      <th>Nombre</th>
+                      <th>Apellidos</th>
+                      <th>Sexo</th>
+                      <th>Fecha Nacimiento</th>
+                      <th>Dirección</th>
+                      <th>NIT</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {values.map(cliente => (
+                      <tr keys={cliente.id}>
+                        <td>{cliente.persona.nombres}</td>
+                        <td>{cliente.persona.apellidos}</td>
+                        <td>{cliente.persona.sexo}</td>
+                        <td>{cliente.persona.fechaNacimiento}</td>
+                        <td>{cliente.persona.direccion}</td>
+                        <td>{cliente.persona.nit}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <th>Nombres</th>
+                      <th>Apellidos</th>
+                      <th>Sexo</th>
+                      <th>Fecha Nacimiento</th>
+                      <th>Dirección</th>
+                      <th>NIT</th>
+                    </tr>
+                  </tfoot>
+                </table>
+              </div>{/* .card */}
+            </div>{/* /col-md-12  donde va  contenido card */}
+          </div>{/* .row */}
+        </div>{/*  .container */}
       </Fragment>
     );
   }
